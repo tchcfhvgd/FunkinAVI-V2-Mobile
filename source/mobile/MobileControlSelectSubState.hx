b3026fa79b691b1d93b4e63979aec2f2d41004ce
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2025-05-18 22:07:36 +0800

    Update MobileControlSelectSubState.hx

--- source/mobile/MobileControlSelectSubState.hx
+++ source/mobile/MobileControlSelectSubState.hx
@@ -62,7 +62,7 @@ class MobileControlSelectSubState extends MusicBeatSubstate
 	{
 		super();
 		
-		PlayState.qqqeb = true;
+		PlayState.qqqeb = false;
 
 		bg = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true,
 			FlxColor.fromRGB(FlxG.random.int(0, 255), FlxG.random.int(0, 255), FlxG.random.int(0, 255)),
